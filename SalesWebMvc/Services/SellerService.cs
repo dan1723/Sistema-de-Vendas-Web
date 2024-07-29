981504eddcb0b7d227c4c695b855575b59c129f2
Danilo F Gomes <dan.gomes1723@gmail.com>
2024-07-29 17:04:52 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -35,5 +35,22 @@ namespace SalesWebMvc.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj) 
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id)) 
+            {
+                throw new NotFiniteNumberException("Id not found");
+            }
+            try 
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) 
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+        }
     }
 }
