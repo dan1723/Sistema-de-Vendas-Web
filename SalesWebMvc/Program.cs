aea28b3d599cebaeb2185406d3c91569afda44b5
Danilo F Gomes <dan.gomes1723@gmail.com>
2024-08-09 08:34:26 -0300

    Implementing simple search

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 
 
